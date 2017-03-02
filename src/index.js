c578fd9e04cc6be68a72d63d61a4c2d81342a307
lulubun <laurapetitt@gmail.com>
2017-03-02 00:16:43 -0500

    tried rewriting index.js but no go

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import Start from './components/Start'
 import store from './store';
-import Start from './components/Start';
-import './index.css';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () =>
   ReactDOM.render(
     <Provider store={store}>
       <Start />
     </Provider>,
     document.getElementById('root')
-  );
-})
+  ),
+)
